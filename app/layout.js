43405cb0c5c11a3a642cacf34b161ba29e4b274a
Hardik Singh <hsingh.doc04@gmail.com>
2026-03-27 19:46:36 +0530

    fixed favicon issue

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body style={{ 
         margin: 0, 
