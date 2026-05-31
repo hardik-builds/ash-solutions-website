05d41cd19d1127a8c8f385185906d002adedfb99
Hardik Singh <hsingh.doc04@gmail.com>
2026-05-31 15:57:00 +0530

    Complete premium redesign

--- app/pricing/page.js
+++
@@ -1,9 +0,0 @@
-// app/pricing/page.js
-import { generateMetadata } from '@/lib/metadata';
-import PricingClient from './pricing-client';
-
-export const metadata = generateMetadata('pricing');
-
-export default function Pricing() {
-  return <PricingClient />;
-}
